8919b1336e3b47ff778ee992b64112d260153060
Sebin <sebinsuresh@gmail.com>
2021-01-17 05:38:28 -0600

    Separated code for modal into modal.js, added device types faux-enum

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-// The plus button on the screen was clicked
-function addPlusClicked() {
-  showAddModal();
-}
-
-// The X button on Add device modal was clicked
-function modalCloseClicked() {
-  hideAddModal();
-}
-
-// Show the Add Device modal
-function showAddModal() {
-  document.querySelector(".modalbg").classList.remove("invisible");
-}
-
-// Hide the Add Device modal
-function hideAddModal() {
-  document.querySelector(".modalbg").classList.add("invisible");
-}
+// Device types enum
+let deviceTypes = {
+  BULB: 1,
+  LAMP: 2,
+  THERMOMETER: 3,
+};
