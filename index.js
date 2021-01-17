d45ac043edf5f006b1b03d5f445fca9f9ba4583a
Sebin <sebinsuresh@gmail.com>
2021-01-17 06:33:12 -0600

    updating previews

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ let deviceTypes = {
   LAMP: 2,
   THERMOMETER: 3,
 };
+
+let devicePreviews = [];
+let addDevicesRegion = document.querySelector(".modalDevices");
+function initializeAddDevicesRegion() {
+  addDevicesRegion.innerHTML = "";
+  for (deviceType in deviceTypes) {
+    console.log(deviceType, deviceTypes[deviceType]);
+    let previewElem = document.createElement("div");
+    previewElem.classList.add("devicePreview");
+
+    addDevicesRegion.appendChild(previewElem);
+  }
+  console.log(addDevicesRegion);
+}
+initializeAddDevicesRegion();
