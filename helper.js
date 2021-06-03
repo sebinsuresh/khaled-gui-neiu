11e06b4a12dd17f8ed54bb8d6d26f32eb2eb0016
Sebin <sebinsuresh@gmail.com>
2021-06-03 11:13:55 -0500

    added more colors

--- helper.js
+++ helper.js
@@ -8,16 +8,24 @@ let TAU = Zdog.TAU;
 // Initialization for Zfont - text in Zdog Illustrations
 Zfont.init(Zdog);
 
-let blue0 = "#0C1220";
-let blue1 = "#273348";
-let blue2 = "#405566";
-let blue3 = "#81B3B9";
-let yellow = "#DFCF82";
-let strongRed = "#CD2F10";
-let bulbYellow = "#E0CB23";
-let offWhite = "#F8F9E8";
-let darkBlue = "#082B5C";
-let lightBlue = "#8BA6BB";
+const blue0 = "#0C1220";
+const blue1 = "#273348";
+const blue2 = "#405566";
+const blue3 = "#81B3B9";
+const yellow = "#DFCF82";
+const strongRed = "#CD2F10";
+const bulbYellow = "#E0CB23";
+const offWhite = "#F8F9E8";
+const darkBlue = "#082B5C";
+const lightBlue = "#8BA6BB";
+const green = "#1B998B";
+const greenDark = "#157A6E";
+const dark = "#333745";
+const dark2 = "#454B5E";
+const yellow = "#ECC05B";
+const grayDark = "#ABB4C4";
+const gray = "#CED3DC";
+const grayLight = "#FBFFFE";
 
 // Converts a hex color code string to 'rgba(..)' string
 function hexToRgba(hex, alpha = "1.0") {
