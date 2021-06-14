829c969bf057c9a1c74037fbca3db8c102e73e8b
Sebin <sebinsuresh@gmail.com>
2021-06-14 16:01:45 -0500

    create object that maps devices to their name strings

---
+++ deviceNames.js
@@ -0,0 +1,14 @@
+/* 
+  Device Types mapped to the name strings, for each device.
+  
+  Example usage:
+  DeviceNames["RPI"] -> to get "Raspberry Pi"
+*/
+export const DeviceNames = {
+  RPI: "Raspberry Pi",
+  LED: "LED Bulb",
+  TEMPSENSOR: "Temp sensor",
+  BULB: "Bulb",
+  LAMP: "Lamp",
+  THERMO: "Thermometer",
+};
