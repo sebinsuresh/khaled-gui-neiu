2eceb2958916608b65ed967ee70fe62dbf16bbe3
Sebin <sebinsuresh@gmail.com>
2021-06-14 21:35:01 -0500

    add color and TAU for Zdog illustration help

---
+++ helpers.js
@@ -0,0 +1,31 @@
+/* 
+  Some helper objects or functions useful for illustrations.
+*/
+
+// TAU: constant used in illustrations
+export const TAU = Zdog.TAU;
+
+// Various colors used by the illustrations.
+export const colors = {
+  blue0: "#0C1220",
+  blue1: "#273348",
+  blue2: "#405566",
+  blue3: "#81B3B9",
+  yellow: "#DFCF82",
+  strongRed: "#CD2F10",
+  bulbYellow: "#E0CB23",
+  offWhite: "#F8F9E8",
+  darkBlue: "#082B5C",
+  lightBlue: "#8BA6BB",
+  green: "#1B998B",
+  greenDark: "#157A6E",
+  dark: "#333745",
+  dark2: "#454B5E",
+  yellowPi: "#ECC05B",
+  grayDark: "#ABB4C4",
+  gray: "#CED3DC",
+  grayLight: "#FBFFFE",
+  red: "#E94F37",
+  redLight: "#F99D8A",
+  redLight2: "#F7A1BB",
+};
