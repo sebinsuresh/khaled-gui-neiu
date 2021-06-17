b0496415baeeb3a4f54d027ab672e6d3ef346a79
Sebin <sebinsuresh@gmail.com>
2021-06-16 21:05:50 -0500

    Add interactjs listeners, warn when dotted region has no ID

--- spaceManager.js
+++ spaceManager.js
@@ -25,6 +25,9 @@ export default class SpaceManager {
     around in, etc.
     */
     this.vizSpaceElement = document.querySelector(selector);
+    if (!this.vizSpaceElement.id) {
+      console.error("The visualizer region element needs to have a unique id");
+    }
 
     /* 
     Array that contains instances of classes representing each device
@@ -33,8 +36,37 @@ export default class SpaceManager {
     */
     this.devices = [];
 
-    // TODO: Make .draggable class elements draggable using interact.js
+    this.makeDraggables();
+
     // TODO: Other event listeners
+  }
+
+  // Make .draggable class elements draggable using interact.js
+  makeDraggables() {
+    interact(`#${this.vizSpaceElement.id} .draggable`).draggable({
+      autoscroll: false,
+      ignoreFrom: `#${this.vizSpaceElement.id} .draggable :not(canvas)`,
+      inertia: false,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: "parent",
+          endOnly: false,
+        }),
+      ],
+      listeners: {
+        move: this.dragMoveListener,
+        end: (ev) => {
+          // The dragEndListener method requires "this" context access -
+          // So that it can access this.devices[].
+          // To pass the context when the event calls the function, use .apply()
+          // like this, instead of simply saying "end: this.dragEndListener"
+          //
+          // Read more about .apply():
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+          this.dragEndListener.apply(this, [ev]);
+        },
+      },
+    });
   }
 
   // Add a device to the smart devices space, and returns the
@@ -74,5 +106,57 @@ export default class SpaceManager {
     this.devices.forEach((devObj) => {
       devObj.show();
     });
+  }
+
+  // Listens to move events thrown by the interactable object
+  dragMoveListener(event) {
+    let target = event.target;
+    let x = Math.round(
+      (parseFloat(target.getAttribute("data-x")) || 0) + event.dx
+    );
+    let y = Math.round(
+      (parseFloat(target.getAttribute("data-y")) || 0) + event.dy
+    );
+
+    target.style.transform = `translate(${x}px, ${y}px)`;
+
+    target.setAttribute("data-x", x);
+    target.setAttribute("data-y", y);
+
+    // TODO: Lines connecting devices?
+    // redrawCanvas();
+  }
+
+  // Function that gets called when an object drag ends.
+  // Sets the x & y positions in device objects using devX & devY from here.
+  // The x & y values are values between 0 and 1, representing it's position
+  // within the device space/dotted region - this might be useful for setting
+  // the position of the html div, irrespective of resolution/window size.
+  dragEndListener(event) {
+    const deviceDiv = event.target;
+    const deviceId = deviceDiv.id;
+
+    // Find the object for this device from the devices array.
+    const deviceObj = this.devices.find((dev) => dev.id == deviceId);
+
+    let devX = Math.abs(
+      (
+        (parseFloat(deviceDiv.getAttribute("data-x")) || 0) /
+        deviceDiv.parentElement.clientWidth
+      ).toFixed(2)
+    );
+    let devY = Math.abs(
+      (
+        (parseFloat(deviceDiv.getAttribute("data-y")) || 0) /
+        deviceDiv.parentElement.clientHeight
+      ).toFixed(2)
+    );
+
+    // Update the object's x & y properties.
+    deviceObj.x = devX;
+    deviceObj.y = devY;
+
+    // TODO: Lines connecting devices?
+    // redrawCanvas();
   }
 }
